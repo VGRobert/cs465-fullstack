a3b5e52eddebb8ec6366e7357d7895f8cfde0b1c
Robert Kratzke <37344483+VGRobert@users.noreply.github.com>
2023-07-09 22:06:59 -0500

    Module 3 changes

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ const app = express();
 const exphbs = require('express-handlebars');
 
 const hbs = exphbs.create({
-    defaultLayout: 'main',
+    extname: 'hbs',
+    defaultLayout: false,
     partialsDir: path.join(__dirname, 'app_server', 'views', 'partials')
 });
 
 // view engine setup
 app.engine('hbs', hbs.engine);
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
